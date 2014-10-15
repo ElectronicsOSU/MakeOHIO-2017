8fa640ea88c85162962546ea9f8f081a9ca0c1b8
Marwan Nakhaleh <=>
2014-10-15 19:23:40 -0400

    the circuit design app is no longer responsive, but at least images work now

--- window.js
+++ window.js
@@ -12,20 +12,22 @@ window.onload = function(){
 	tcon = tcan.getContext('2d');
 	//resize canvases whenever window changes
 	window.addEventListener('resize', resizeCanvas, false);
-	window.addEventListener('orientationchange', resizeCanvas, false);
-	resizeCanvas();
 	var and = new Image();
 	and.src = "and.png";
 	var or = new Image();
 	or.src = "or.png";
-	tcon.drawImage(and, 10, 10);
-	tcon.drawImage(or, 10, 60);
+	and.onload = function(){
+		tcon.drawImage(and, 10, 10);
+	}
+	or.onload = function(){
+		tcon.drawImage(or, 10, 60);
+	}
 };
 
 //resize on event
 function resizeCanvas(){
-	ccan.width = window.innerWidth * .65;
-	ccan.height = window.innerHeight * .8;
-	tcan.width = window.innerWidth * .25;
-	tcan.height = window.innerHeight * .8;
+	ccan.width = 800;
+	ccan.height = 600;
+	tcan.width = 200;
+	tcan.height = 600;
 }
