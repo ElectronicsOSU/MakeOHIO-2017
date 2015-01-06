0c776b2c31eb3ed5feeef880ed608508991b1680
egladman <eli.gladman@gmail.com>
2015-01-05 20:00:12 -0500

    polished site

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,20 @@ $(window).scroll(function() {
 
 
 
+/* Calendar */
+
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    header: {
+    	left: 'prev,next today',
+    	center: 'title',
+    	right: 'month,agendaWeek,agendaDay'
+    },
+    googleCalendarApiKey: 'AIzaSyBu5kL2kgj9AZXCM8kfSLukPx1Vb7bRSh4',
+    events: {
+      googleCalendarId: 'electronicsosu@gmail.com',
+      timezone: 'America/New_York'
+    }
+  });
+});
